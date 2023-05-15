768cf0e80d644aaa371e3c3cdbe31a47599c8fcb
Damir <damir2004damir@mail.ru>
2023-05-15 13:08:40 +0300

    F2

--- Assets/Fruits.cs
+++ Assets/Fruits.cs
@@ -5,11 +5,14 @@ using UnityEngine;
 public class Fruits : MonoBehaviour
 {   
     [SerializeField] private GameObject[] Objects;
+    [SerializeField] private FruitsHandler.Type type;
     private Vector3[] _defoultPosition = new Vector3[3];
+    private FruitsHandler _fruitsHandler;
 
     public int FruitsCount;
     private void OnEnable()
     {
+        _fruitsHandler = FindObjectOfType<FruitsHandler>();
         for (int i = 0; i < Objects.Length; i++)
         {
             Objects[i].SetActive(true);
@@ -25,6 +28,7 @@ public class Fruits : MonoBehaviour
             if (Objects[i] != null && (_defoultPosition[i] - Objects[i].transform.position).magnitude > 0.1f)
             {
                 Destroy(Objects[i]);
+                _fruitsHandler.AddFruits(1, type);
                 FruitsCount = CheckFruitsCount();
             }  
     }
